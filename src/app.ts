44780ab58038f7402ca635c577de3b7b32ad945f
FahimHasan683031 <fahimhasan683031@gmail.com>
2026-01-11 07:05:33 +0600

    chore: update author name and allowed origins

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const allowedOrigins = [
   'http://goroqit.com',        
   'http://localhost:3000',
   'http://localhost:3001',
-  'http://localhost:3002',
+  'https://fahim_hasan5000.binarybards.online',
   'http://localhost:3003',
   'http://localhost:3004',
   'http://localhost:3005',
